5b3a88f6e5bde40ec935022696cd0d2e622a6f4d
Artemisia Dutra <artemisiadutra2015@gmail.com>
2023-05-30 23:36:52 -0300

    App com paginação

---
+++ App/flutter_application_1/lib/third_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ThirdPage extends StatefulWidget {
+  const ThirdPage({super.key});
+
+  @override
+  State<ThirdPage> createState() => _ThirdPageState();
+}
+
+class _ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Serviços"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+
+              children: [
+                Image.asset('assets/images/detalhe_servico.png'),
+                Text(
+                  'Nossos Serviços',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+              ]),
+              
+            ),
+
+
+            Row(
+   
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              children: [
+                Text("Consultoria"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Cálculo de preços"),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text("Acompanhamento de projetos"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
